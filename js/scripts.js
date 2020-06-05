0b782612b0823d5ee8ed717232b590c5031f7493
heyimmatt <mrmcfarland@gmail.com>
2020-06-05 11:40:35 -0700

    Use hide class for hiding content, update jQuery to use the hide class, update return results so they can be used in jQuery selector, add return object when there are two results with the same count, and add if/else logic for handling what to display when there are two results with the same count.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
-$(document).ready(function() {
-  $("form#language-survey").submit(function(event) {
+$(document).ready(function () {
+  $('form#language-survey').submit(function (event) {
     event.preventDefault();
     const answers = [
-      $("#question1").val(),
-      $("#question2").val(),
-      $("#question3").val(),
-      $("#question4").val(),
-      $("#question5").val(),
+      $('#question1').val(),
+      $('#question2').val(),
+      $('#question3').val(),
+      $('#question4').val(),
+      $('#question5').val(),
     ];
     const result = calculateResults(answers);
-    $("#results").append(result);
-    $("#results").removeClass("hide");
-  })
-  $(":reset").click(function() {
-    $("#results").addClass("hide");
-  })
-})
+
+    if (result.idSelector) {
+      $(`#${result.idSelector}`)
+        .append(`<p>${result.pTagText}</p>`)
+        .removeClass('hide');
+    } else $(`#${result}`).removeClass('hide');
+  });
+
+  $(':reset').click(function () {
+    $('#ruby, #javascript, #csharp, #python').addClass('hide');
+  });
+});
 
 function calculateResults(answerArray) {
   let aCount = 0;
   let bCount = 0;
   let cCount = 0;
   let dCount = 0;
-  
+
   answerArray.forEach((answer) => {
     switch (answer) {
       case 'a':
@@ -39,15 +44,19 @@ function calculateResults(answerArray) {
         break;
     }
   });
-  
-  if (aCount > bCount && aCount > cCount && aCount > dCount) {
-    return 'You should learn Ruby!';
-  } else if (bCount > aCount && bCount > cCount && bCount > dCount) {
-    return 'You should learn JavaScript!';
-  } else if (cCount > aCount && cCount > bCount && cCount > dCount) {
-    return 'You should learn C#!';
-  } else if (dCount > aCount && dCount > bCount && dCount > cCount) {
-    return 'You should learn Python!';
-  } else return 'Your answers suggest you want to learn two languages! Since you\'re indecisive, I suggest you learn a strongly typed language like C#.'
-}
 
+  if (aCount > bCount && aCount > cCount && aCount > dCount) {
+    return 'ruby';
+  } else if (bCount > aCount && bCount > cCount && bCount > dCount) {
+    return 'javascript';
+  } else if (cCount > aCount && cCount > bCount && cCount > dCount) {
+    return 'csharp';
+  } else if (dCount > aCount && dCount > bCount && dCount > cCount) {
+    return 'python';
+  } else
+    return {
+      idSelector: 'csharp',
+      pTagText:
+        "Your answers suggest you want to learn two languages! Since you're indecisive, I suggest you learn a strongly typed language!",
+    };
+}
